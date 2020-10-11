29393a217dde3bf23510509da2847b59c3bf1976
thcl-0407 <thcloc@icloud.com>
2020-10-11 16:51:38 +0700

    update ui nhap ma otp, them chuc nang gui lai code neu loi

--- app/src/main/java/com/example/appchat/NhapMaOTPActivity.java
+++ app/src/main/java/com/example/appchat/NhapMaOTPActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -32,6 +36,7 @@ import com.google.firebase.auth.PhoneAuthProvider;
 import java.util.concurrent.TimeUnit;
 
 public class NhapMaOTPActivity extends AppCompatActivity {
+    int Count_Send_Code = 1;
     FirebaseAuth auth;
 
     private String SoDienThoai_XacThuc;
@@ -42,13 +47,18 @@ public class NhapMaOTPActivity extends AppCompatActivity {
 
     EditText etxtMaOTP_DangKy;
     Button btnTiepTuc_MaOTP_DangKy;
+    Button btnResend_Code_DangKy;
     ImageButton btnBack_OTP;
 
     TextView txt_notify_sending_otp;
+    TextView txt_notify_sent_otp;
     TextView txtSoDienThoai_XacThuc;
+    TextView txt_suggest_nhap_ma_otp;
+    TextView txt_suggest_timer_otp;
 
     ProgressBar prgbr_Waiting_OTP_DangKy;
     LinearLayout container_nhap_ma_xac_thuc;
+    LinearLayout container_err_sms_sending;
 
     PhoneAuthCredential credential;
 
@@ -65,6 +75,7 @@ public class NhapMaOTPActivity extends AppCompatActivity {
         Init_Data();
         btnTiepTuc_MaOTP_DangKy_Click();
         btnBack_OTP_Click();
+        btnResend_Code_DangKy_Click();
 
         HoTen_DangKy = getIntent().getExtras().getString("HoTen_DangKy");
         SoDienThoai_XacThuc = "+84" + getIntent().getExtras().getString("SoDienThoai_DangKy");
@@ -74,21 +85,26 @@ public class NhapMaOTPActivity extends AppCompatActivity {
         btnBack_OTP.setEnabled(false);
     }
 
-    protected void Init_Data(){
+    protected void Init_Data() {
         etxtMaOTP_DangKy = (EditText) findViewById(R.id.etxtMaOTP_DangKy);
         btnTiepTuc_MaOTP_DangKy = (Button) findViewById(R.id.btnTiepTuc_MaOTP_DangKy);
         btnBack_OTP = (ImageButton) findViewById(R.id.btnBack_DangKy_OTP);
+        btnResend_Code_DangKy = (Button) findViewById(R.id.btnResend_Code_DangKy);
 
         txtSoDienThoai_XacThuc = (TextView) findViewById(R.id.txtSoDienThoai_XacThuc);
         txtSoDienThoai_XacThuc.setText("0" + getIntent().getExtras().getString("SoDienThoai_DangKy"));
 
         txt_notify_sending_otp = (TextView) findViewById(R.id.txt_notify_sending_otp);
+        txt_notify_sent_otp = (TextView) findViewById(R.id.txt_notify_sent_otp);
+        txt_suggest_nhap_ma_otp = (TextView) findViewById(R.id.txt_suggest_nhap_ma_otp);
+        txt_suggest_timer_otp = (TextView) findViewById(R.id.txt_suggest_timer_otp);
 
         container_nhap_ma_xac_thuc = (LinearLayout) findViewById(R.id.container_nhap_ma_xac_thuc);
         prgbr_Waiting_OTP_DangKy = (ProgressBar) findViewById(R.id.prgbr_Waiting_OTP_DangKy);
+        container_err_sms_sending = (LinearLayout) findViewById(R.id.container_err_sms_sending);
     }
 
-    protected void btnBack_OTP_Click(){
+    protected void btnBack_OTP_Click() {
         btnBack_OTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,7 +113,43 @@ public class NhapMaOTPActivity extends AppCompatActivity {
         });
     }
 
-    protected void btnTiepTuc_MaOTP_DangKy_Click(){
+    protected void  btnResend_Code_DangKy_Click(){
+        btnResend_Code_DangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Count_Send_Code == 2){
+                    prgbr_Waiting_OTP_DangKy.setVisibility(View.VISIBLE);
+                    txt_notify_sending_otp.setText("Đăng Ký Mới");
+                    txt_notify_sending_otp.setVisibility(View.VISIBLE);
+                    container_nhap_ma_xac_thuc.setVisibility(View.GONE);
+
+                    Intent intent = new Intent(NhapMaOTPActivity.this, DangKyHoTenActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                container_nhap_ma_xac_thuc.setVisibility(View.GONE);
+                txt_notify_sent_otp.setTextColor(Color.rgb(0, 0, 0));
+                txt_notify_sent_otp.setText("Đã Gửi Mã Xác Nhận Đến Số");
+                txtSoDienThoai_XacThuc.setTextColor(Color.rgb(0, 0, 0));
+                txt_suggest_nhap_ma_otp.setText("Xin vui lòng kiểm tra SMS và điền mã xác nhận bên dưới");
+                etxtMaOTP_DangKy.setVisibility(View.VISIBLE);
+                container_err_sms_sending.setVisibility(View.GONE);
+                btnTiepTuc_MaOTP_DangKy.setVisibility(View.VISIBLE);
+
+                prgbr_Waiting_OTP_DangKy.setVisibility(View.VISIBLE);
+                txt_notify_sending_otp.setText("Đang Gửi Lại Mã Xác Nhận Mới");
+                txt_notify_sending_otp.setVisibility(View.VISIBLE);
+
+                resend_sms_code_otp(SoDienThoai_XacThuc);
+
+                //Đếm Số Lần Gửi Code Của Người Dùng
+                Count_Send_Code = Count_Send_Code + 1;
+            }
+        });
+    }
+
+    protected void btnTiepTuc_MaOTP_DangKy_Click() {
         btnTiepTuc_MaOTP_DangKy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -115,21 +167,116 @@ public class NhapMaOTPActivity extends AppCompatActivity {
         });
     }
 
-    protected void request_OTP(String phone) {
-        auth.setLanguageCode("vi");
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(phone, 30, TimeUnit.SECONDS,
-                NhapMaOTPActivity.this,
+    protected void resend_sms_code_otp(String phone){
+        btnResend_Code_DangKy.setEnabled(false);
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phone, 60, TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
                 new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
                     @Override
                     public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                        if(phoneAuthCredential.getSmsCode() != null){
+                        if (phoneAuthCredential.getSmsCode() != null) {
                             etxtMaOTP_DangKy.setText(phoneAuthCredential.getSmsCode());
                         }
                     }
 
                     @Override
                     public void onVerificationFailed(@NonNull FirebaseException e) {
+                        prgbr_Waiting_OTP_DangKy.setVisibility(View.GONE);
+                        txt_notify_sending_otp.setVisibility(View.GONE);
+                        container_nhap_ma_xac_thuc.setVisibility(View.VISIBLE);
 
+                        //Thông Báo Lỗi
+                        txt_notify_sent_otp.setTextColor(Color.rgb(255, 0, 0));
+                        txt_notify_sent_otp.setText("Có Lỗi Xảy Ra Khi Gửi SMS Đến Số");
+                        txtSoDienThoai_XacThuc.setTextColor(Color.rgb(255, 0, 0));
+                        txt_suggest_nhap_ma_otp.setText("");
+                        txt_suggest_timer_otp.setVisibility(View.VISIBLE);
+                        etxtMaOTP_DangKy.setVisibility(View.GONE);
+                        container_err_sms_sending.setVisibility(View.VISIBLE);
+                        btnTiepTuc_MaOTP_DangKy.setVisibility(View.GONE);
+
+                        CountDownTimer timer = new CountDownTimer(10 * 1000, 1000) {
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+                                btnResend_Code_DangKy.setText(Long.toString(millisUntilFinished/1000) + " s");
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                if(Count_Send_Code == 2){
+                                    btnResend_Code_DangKy.setText("Thử Đăng Ký Lại Với Số Điện Thoại Khác");
+                                    btnResend_Code_DangKy.setEnabled(true);
+                                    txt_suggest_timer_otp.setVisibility(View.GONE);
+                                    return;
+                                }
+
+                                btnResend_Code_DangKy.setText("Gửi Lại Mã Xác Nhận Mới");
+                                btnResend_Code_DangKy.setEnabled(true);
+                                txt_suggest_timer_otp.setVisibility(View.GONE);
+                            }
+                        }.start();
+
+                        btnBack_OTP.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        VerificationId = s;
+                        ResendToken = forceResendingToken;
+
+                        prgbr_Waiting_OTP_DangKy.setVisibility(View.GONE);
+                        txt_notify_sending_otp.setVisibility(View.GONE);
+                        container_nhap_ma_xac_thuc.setVisibility(View.VISIBLE);
+                        btnBack_OTP.setEnabled(true);
+                    }
+                }, ResendToken);
+    }
+
+    protected void request_OTP(String phone) {
+        btnResend_Code_DangKy.setEnabled(false);
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phone, 60, TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        if (phoneAuthCredential.getSmsCode() != null) {
+                            etxtMaOTP_DangKy.setText(phoneAuthCredential.getSmsCode());
+                        }
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        prgbr_Waiting_OTP_DangKy.setVisibility(View.GONE);
+                        txt_notify_sending_otp.setVisibility(View.GONE);
+                        container_nhap_ma_xac_thuc.setVisibility(View.VISIBLE);
+
+                        //Thông Báo Lỗi
+                        txt_notify_sent_otp.setTextColor(Color.rgb(255, 0, 0));
+                        txt_notify_sent_otp.setText("Có Lỗi Xảy Ra Khi Gửi SMS Đến Số");
+                        txtSoDienThoai_XacThuc.setTextColor(Color.rgb(255, 0, 0));
+                        txt_suggest_nhap_ma_otp.setText("");
+                        txt_suggest_timer_otp.setVisibility(View.VISIBLE);
+                        etxtMaOTP_DangKy.setVisibility(View.GONE);
+                        container_err_sms_sending.setVisibility(View.VISIBLE);
+                        btnTiepTuc_MaOTP_DangKy.setVisibility(View.GONE);
+
+                        CountDownTimer timer = new CountDownTimer(10 * 1000, 1000) {
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+                                btnResend_Code_DangKy.setText(Long.toString(millisUntilFinished/1000) + " s");
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                btnResend_Code_DangKy.setText("Gửi Lại Mã Xác Nhận Mới");
+                                btnResend_Code_DangKy.setEnabled(true);
+                                txt_suggest_timer_otp.setVisibility(View.GONE);
+                            }
+                        }.start();
+
+                        btnBack_OTP.setEnabled(true);
                     }
 
                     @Override
@@ -145,18 +292,18 @@ public class NhapMaOTPActivity extends AppCompatActivity {
                 });
     }
 
-    protected void SignInWithCredential(final PhoneAuthCredential credential){
+    protected void SignInWithCredential(final PhoneAuthCredential credential) {
         auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Intent intent_03 = new Intent(NhapMaOTPActivity.this, DangKyMatKhauActivity.class);
                     intent_03.putExtra("SoDienThoai_DangKy", SoDienThoai_XacThuc);
                     intent_03.putExtra("HoTen_DangKy", HoTen_DangKy);
 
                     startActivity(intent_03);
                     finish();
-                }else {
+                } else {
                     prgbr_Waiting_OTP_DangKy.setVisibility(View.GONE);
                     txt_notify_sending_otp.setVisibility(View.GONE);
                     container_nhap_ma_xac_thuc.setVisibility(View.VISIBLE);
